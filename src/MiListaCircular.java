1e2dd0973efa98f4498b421825714a270d11a941
Santiagosmx <santiagocely175@gmail.com>
2026-09-17 19:09:01 -0500

    a

--- src/MiListaCircular.java
+++ src/MiListaCircular.java
@@ -47,7 +47,7 @@ public class MiListaCircular implements ListInterface {
 
     @Override
     public Object get(Node node) {
-        if (cabeza == null) {
+        if (cabeza == null || node == null) {
             return null;
         }
 
@@ -66,7 +66,7 @@ public class MiListaCircular implements ListInterface {
 
     @Override
     public Node search(Object object) {
-        if (cabeza == null) {
+        if (cabeza == null || object == null) {
             return null;
         }
 
@@ -85,25 +85,12 @@ public class MiListaCircular implements ListInterface {
 
     @Override
     public boolean add(Object object) {
-        Node nuevoNodo = new Node(object);
-
-        if (cabeza == null) {
-            cabeza = nuevoNodo;
-            cola = nuevoNodo;
-            nuevoNodo.siguiente = cabeza;
-        } else {
-            nuevoNodo.siguiente = cabeza;
-            cola.siguiente = nuevoNodo;
-            cola = nuevoNodo;
-        }
-
-        size++;
-        return true;
+        return insertTail(object);
     }
 
     @Override
     public boolean insert(Node node, Object object) {
-        if (cabeza == null) {
+        if (cabeza == null || node == null) {
             return false;
         }
 
@@ -179,7 +166,7 @@ public class MiListaCircular implements ListInterface {
 
     @Override
     public boolean set(Node node, Object object) {
-        if (cabeza == null) {
+        if (cabeza == null || node == null) {
             return false;
         }
 
@@ -199,7 +186,7 @@ public class MiListaCircular implements ListInterface {
 
     @Override
     public boolean remove(Node node) {
-        if (cabeza == null) {
+        if (cabeza == null || node == null) {
             return false;
         }
 
@@ -208,31 +195,30 @@ public class MiListaCircular implements ListInterface {
 
         do {
             if (iterador == node) {
-
-                if (iterador == cabeza) {
+                // Si es el único nodo en la lista
+                if (cabeza == cola) {
+                    cabeza = null;
+                    cola = null;
+                }
+                // Si el nodo a eliminar es la cabeza pero hay más elementos
+                else if (iterador == cabeza) {
                     cabeza = cabeza.siguiente;
                     cola.siguiente = cabeza;
-                } else {
+                }
+                // Si el nodo está en el medio o es la cola
+                else {
                     anterior.siguiente = iterador.siguiente;
-
                     if (iterador == cola) {
                         cola = anterior;
                     }
                 }
 
                 size--;
-
-                if (size == 0) {
-                    cabeza = null;
-                    cola = null;
-                }
-
                 return true;
             }
 
             anterior = iterador;
             iterador = iterador.siguiente;
-
         } while (iterador != cabeza);
 
         return false;
@@ -289,15 +275,20 @@ public class MiListaCircular implements ListInterface {
             object = new Object[size];
         }
 
-        Node iterador = cabeza;
-        int i = 0;
-
         if (cabeza != null) {
+            Node iterador = cabeza;
+            int i = 0;
             do {
                 object[i] = iterador.dato;
                 iterador = iterador.siguiente;
                 i++;
             } while (iterador != cabeza);
+
+            // Estándar de Java: si el array proporcionado es más grande que la lista,
+            // el elemento contiguo al final se establece en null
+            if (object.length > size) {
+                object[size] = null;
+            }
         }
 
         return object;
@@ -319,7 +310,6 @@ public class MiListaCircular implements ListInterface {
             if (iterador == to) {
                 break;
             }
-
             iterador = iterador.siguiente;
 
         } while (iterador != from);
@@ -331,10 +321,37 @@ public class MiListaCircular implements ListInterface {
     public MiListaCircular sortList() {
         MiListaCircular nuevaLista = new MiListaCircular();
 
-        Node iterador = cabeza;
+        if (cabeza == null) {
+            return nuevaLista;
+        }
 
-        while (iterador != null) {
-            // ...
+        Node iterador = cabeza;
+        do {
+            nuevaLista.insertTail(iterador.dato);
+            iterador = iterador.siguiente;
+        } while (iterador != cabeza);
+
+        if (nuevaLista.size > 1) {
+            boolean huboIntercambio;
+            do {
+                huboIntercambio = false;
+                Node actual = nuevaLista.cabeza;
+
+                while (actual.siguiente != nuevaLista.cabeza) {
+
+                    Comparable obj1 = (Comparable) actual.dato;
+                    Comparable obj2 = (Comparable) actual.siguiente.dato;
+
+                    if (obj1.compareTo(obj2) > 0) {
+                        Object temp = actual.dato;
+                        actual.dato = actual.siguiente.dato;
+                        actual.siguiente.dato = temp;
+
+                        huboIntercambio = true;
+                    }
+                    actual = actual.siguiente;
+                }
+            } while (huboIntercambio);
         }
 
         return nuevaLista;
